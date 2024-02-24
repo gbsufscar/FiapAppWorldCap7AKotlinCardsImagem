f21ee508c27456ad7df293bba54aced62372aa0e
Gustavo Bernardo da Silva <gbsufscar@hotmail.com>
2024-02-24 09:58:51 -0300

    Incluíndo imágens na aplicação.

--- app/src/main/java/br/com/fiap/imcapp/MainActivity.kt
+++ app/src/main/java/br/com/fiap/imcapp/MainActivity.kt
@@ -1,20 +1,29 @@
 package br.com.fiap.imcapp
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalMapOf
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.fiap.imcapp.ui.theme.IMCAppTheme
@@ -29,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    IMCScreen()
                 }
             }
         }
@@ -39,24 +48,37 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun IMCScreen (){
     Box(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .border(4.dp, Color.Red)
+            .fillMaxWidth()
     ){
         // Coluna 1 dentro da Box
         Column (
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .border(4.dp, Color.Green)
+                .fillMaxWidth()
         ){
             // Coluna 2 - Cabeçalho/Header da Activity (dentro da Coluna 1)
             Column (
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
+                    .border(4.dp, Color.Yellow)
                     .fillMaxWidth()
                     .height(160.dp)
             ){
-                /* TODO */
+                androidx.compose.foundation.Image(
+                    painter = painterResource(id = R.drawable.bmi),
+                    contentDescription ="logo",
+                    modifier = Modifier
+                        .size(60.dp)
+                        .padding(top = 16.dp)
+
+                    )
             }
             // Coluna 3 - Formulário (dendro da Coluna 1 e abaixo da Coluna 2
             Column(
                 modifier = Modifier
+                    .border(4.dp, Color.Black)
                     .fillMaxWidth()
                     .padding(horizontal = 32.dp)
             ){
@@ -77,6 +99,6 @@ fun IMCScreen (){
 @Composable
 fun GreetingPreview() {
     IMCAppTheme {
-        Greeting("Android")
+        IMCScreen()
     }
 }
